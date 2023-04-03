8eb2970b06fb8ff296ccae1015f90d20d4795ab0
Keresifon Ekpo <keresifon.ekpo@gmail.com>
2023-04-03 13:15:10 +0100

    created filePush, implemented implode

---
+++ filePush.php
@@ -0,0 +1,24 @@
+<?php
+
+    if(isset($_POST['Submit'])){
+
+        $fName = $_POST['firstname'];
+
+        $lName = $_POST["lastname"];
+
+        $email = $_POST["email"];
+
+        $city = $_POST["city"];
+
+        $state = $_POST["state"];
+
+        $data = array($fName, $lName, $email, $city, $state);
+
+        $fp =fopen("store.txt","a");
+
+        fwrite($fp, implode(" ", $data));
+    
+        fclose($fp);
+        
+    }
+
--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <title>Document</title>
 </head>
 <body>
-    <Form action="" method="post">
+
+    <Form action="filePush.php" method="post">
         <input type="text" name="firstname" placeholder="First name" required><br>
         <input type="text" name="lastname" placeholder="Last name"required><br>
         <input type="text" name="email" placeholder="e-mail"required><br>
@@ -15,39 +16,10 @@
         <input type="text" name="state" placeholder="State"><br>
         <input type="submit" name="Submit">
     </Form>
-</body>
-</html>
-
-<!-- Save to file -->
-<?php
-
-    if(isset($_POST['Submit'])){
-
-        $fName = $_POST['firstname']. " ";
-
-        $lName = $_POST["lastname"]. " ";
-
-        $email = $_POST["email"]. " ";
-
-        $city = $_POST["city"]. " ";
-
-        $state = $_POST["state"];
-
-        $data = [$fName, $lName, $email, $city, $state];
-
-        $fp =fopen("store.txt","a");
-
-        fwrite($fp, $data[0]);
-        fwrite($fp, $data[1]);
-        fwrite($fp, $data[2]);
-        fwrite($fp, $data[3]);
-        fwrite($fp, $data[4]);
     
-        fclose($fp);
-        
-    }
+</body>
 
-?>
+</html>
 
 <!-- Submit to server -->
 <script>
@@ -65,3 +37,4 @@
     }
 
 <script/>
+
